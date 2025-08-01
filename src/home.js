fb035dc842f233447f40b86b935706dfe7ea679c
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-08-01 06:49:00 +0100

    Add elements dynamically for homepage

---
+++ src/home.js
@@ -0,0 +1,21 @@
+import foodImg from "./images/food.jpg";
+
+export function createHomepage(){
+    let content = document.getElementById("content");
+    let img = document.createElement("img");
+    img.src = foodImg;
+    let div = document.createElement("div");
+    div.classList.add("content-body");
+    let h1 = document.createElement("h1");
+    h1.textContent = "Enjoy our delicous Menu";
+    let p = document.createElement("p");
+    p.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    let button = document.createElement("button");
+    button.innerHTML = "View Our Menu";
+    button.classList.add("menu-button");
+    div.appendChild(h1);
+    div.appendChild(p);
+    div.appendChild(button);
+    content.appendChild(img);
+    content.appendChild(div);
+}
