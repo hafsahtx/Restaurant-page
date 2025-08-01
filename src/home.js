6952b2048032fde36801708629253358a61c8949
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-08-01 16:48:08 +0100

    Set alt and height attributes for img

--- src/home.js
+++ src/home.js
@@ -4,6 +4,9 @@ export function createHomepage(){
     let content = document.getElementById("content");
     let img = document.createElement("img");
     img.src = foodImg;
+    img.classList.add("background-img");
+    img.setAttribute("alt","platter of food");
+    img.setAttribute("height","600px")
     let div = document.createElement("div");
     div.classList.add("content-body");
     let h1 = document.createElement("h1");
