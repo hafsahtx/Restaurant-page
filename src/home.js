ecb578095312bbffb140dd8ee21ba0850bf04ee6
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-08-05 10:30:34 +0100

    remove button from homepage

--- src/home.js
+++ src/home.js
@@ -6,19 +6,14 @@ export function createHomepage(){
     img.src = foodImg;
     img.classList.add("background-img");
     img.setAttribute("alt","platter of food");
-    img.setAttribute("height","600px")
     let div = document.createElement("div");
     div.classList.add("content-body");
     let h1 = document.createElement("h1");
     h1.textContent = "Enjoy our delicous Menu";
     let p = document.createElement("p");
     p.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-    let button = document.createElement("button");
-    button.innerHTML = "View Our Menu";
-    button.classList.add("menu-button");
     div.appendChild(h1);
     div.appendChild(p);
-    div.appendChild(button);
     content.appendChild(img);
     content.appendChild(div);
 }
