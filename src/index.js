ffa7745e7d229dd39a132c2642ceedf3006e4753
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-08-05 10:32:07 +0100

    import about us and add listener to button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import { createHomepage } from "./home";
 import { createMenuPage } from "./menu";
+import { createAboutUsPage } from "./about";
 
 createHomepage();
 let home_button = document.querySelector(".home");
 let menu_button = document.querySelector(".menu");
+let about_button = document.querySelector(".about");
 let content = document.getElementById("content");
 menu_button.addEventListener("click",()=>{
     content.innerHTML = "";
@@ -13,4 +15,8 @@ menu_button.addEventListener("click",()=>{
 home_button.addEventListener("click",()=>{
     content.innerHTML = "";
     createHomepage();
+});
+about_button.addEventListener("click",()=>{
+    content.innerHTML = "";
+    createAboutUsPage();
 });
